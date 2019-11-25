8c63256afe0459a42ed87428560f3ca753f81e7d
ceciliaGeraldo <ceciliandrea.geraldo@gmail.com>
2019-11-25 10:45:07 -0300

    Added path resolve and normalize for relative ones or typing errors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 //leer archivo.md
 const readMd = (path => {
@@ -8,8 +9,26 @@ const readMd = (path => {
         reject(new Error("¡Ouch! No se ha encontrado el archivo " + path))
       }
       resolve(data)
-      //console.log(data)
+      console.log(data)
     })
 
   })
 })
+
+//ruta ingresada por el usuario en consola
+let routeFile = process.argv[2];
+ // convierte en ruta absoluta la ruta ingresada (path.resolve)
+routeFile = path.resolve(routeFile);
+ //si existen errores de escritura, lo normaliza para leerlo con éxito (path.normalize)
+routeFile = path.normalize(routeFile);
+
+readMd(routeFile);
+
+/*
+if(require.main===module){
+  readMd(process.argv[2].then(console.log))
+}else{
+  module.exports=readMd;
+}
+
+*/
